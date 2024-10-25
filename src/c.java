5e91030c1fec5a8f23ee7405ce65f21045ab7e4d
BT_1N3_03 <sample@bths.edu>
2024-10-25 10:32:34 -0400

    a

--- src/c.java
+++ src/c.java
@@ -2,5 +2,6 @@ public class c {
     public static void main(String[] args) {
         System.out.println("never gonna give");
         System.out.println(2+2);
+        System.out.println(1);
     }
 }
