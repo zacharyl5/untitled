b0404c0afbec7a2bb43e6bcb168b87b9fcbf0e26
BT_1N3_03 <sample@bths.edu>
2024-10-25 10:02:17 -0400

    added more print

--- src/b.java
+++ src/b.java
@@ -1,5 +1,6 @@
 public class b {
     public static void main(String[] args) {
-        System.out.println("BLAH");
+        System.out.println("BLAH!");
+        System.out.println("not blah");
     }
 }
